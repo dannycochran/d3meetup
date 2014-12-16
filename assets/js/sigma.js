3d6b422dcfe1d229bedbd3533a34aa13ec2aa5e7
Cindy Zhang <cindy.zhang@ayasdi.com>
2014-12-16 11:28:42 -0800

    rendering networks in sigma

--- assets/js/sigma.js
+++ assets/js/sigma.js
@@ -1,5 +1,6 @@
 var sigmaGrapher = function () {
   var grapher = {
+    colors: ['#d73027', '#f46d43', '#fdae61', '#fee090', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#FFFFFF'],
     initialize: function () {
       this.$el = $('div.network-container');
       this.$header = $('header');
@@ -8,49 +9,48 @@ var sigmaGrapher = function () {
     },
 
     render: function (data) {
+      this.data = data;
       this.$el.empty();
-      this.$header.find('span').html('Network with Sigma.js (' + data.nodes + ' nodes & ' + data.edges + ' links)');
-      var i,
-          s,
-          N = data.nodes,
-          E = data.edges,
-          g = {
-            nodes: [],
-            edges: []
-          };
+      this.$header.find('span').html('Network with Sigma.js (' + data.nodes.length + ' nodes & ' + data.links.length + ' links)');
+      
+      // modify data to have a radius
+      _.each(data.nodes, function (n, i) {
+        n.size = 1 + n.row_ids.length;
+      });
 
-      // Generate a random graph:
-      for (i = 0; i < N; i++)
-        g.nodes.push({
-          id: 'n' + i,
-          label: 'Node ' + i,
-          x: Math.random(),
-          y: Math.random(),
-          size: Math.random(),
-          color: '#666'
-        });
+      // create edges
+      data.edges = data.links;
+      _.each(data.edges, function (e, i) {
+        e.id = i.toString(10);
+        e.source = e.from.toString(10);
+        e.target = e.to.toString(10);
+        e.color = '#ccc';
+      });
 
-      for (i = 0; i < E; i++)
-        g.edges.push({
-          id: 'e' + i,
-          source: 'n' + (Math.random() * N | 0),
-          target: 'n' + (Math.random() * N | 0),
-          size: Math.random(),
-          color: '#ccc'
-        });
+      this.updateColorings();
+
       sigma.renderers.def = sigma.renderers.webgl;
       // Instantiate sigma:
       s = new sigma({
-        graph: g,
+        graph: data,
         container: this.$el.get(0),
-        type: 'webgl'
+        type: 'webgl',
+        settings: {
+          drawLabels: false
+        }
       });
 
       return this;
     },
 
+    updateColorings: function () { // random colors
+      _.each(this.data.nodes, function (n) {
+        n.color = this.colors[Math.floor(Math.random() * 8)];
+      }.bind(this));
+    },
+
     toggleNetwork: function (e) {
-      var networkData = $(e.currentTarget).attr('id') === 'small' ? {nodes:100, edges: 1000} : {nodes:5000, edges: 32000};
+      var networkData = $(e.currentTarget).attr('id') === 'small' ? smallNetwork : largeNetwork;
       this.render(networkData);
     },
   };
@@ -60,5 +60,5 @@ var sigmaGrapher = function () {
 
 $(document).ready(function () {
   var grapher = new sigmaGrapher();
-  grapher.render({nodes:100, edges: 1000});
+  grapher.render(smallNetwork);
 });
